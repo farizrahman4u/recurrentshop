f41b955b56d3476e0b57243bb57954c5028e17eb
farizrahman4u <farizrahman4u@gmail.com>
2017-04-30 02:49:25 +0530

    bug fix

--- recurrentshop/engine.py
+++ recurrentshop/engine.py
@@ -39,7 +39,7 @@ class _OptionalInputPlaceHolder(Layer):
 
 
 def _get_cells():
-    from .cells import *
+    from .cells import SimpleRNNCell, LSTMCell, GRUCell
     cells = {}
     cells['SimpleRNNCell'] = SimpleRNNCell
     cells['LSTMCell'] = LSTMCell
