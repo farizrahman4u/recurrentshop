618612919f09e4c46068988de310f3ae336b9d92
Abhai Kollara <abhai.dilip@gmail.com>
2017-06-06 15:35:29 +0530

    Rewrite tests to use pytest

--- tests/test_recurrent_sequential.py
+++ tests/test_recurrent_sequential.py
@@ -1,250 +1,281 @@
-from recurrentshop import *
-from keras.models import *
-from keras.layers import *
+from recurrentshop import RecurrentSequential
+from recurrentshop.cells import *
+from keras.models import Model
+from keras.layers import Activation, Dense, Input
+from keras.utils.test_utils import keras_test
+import numpy as np
 
 
-rnn = RecurrentSequential()
-rnn.add(LSTMCell(7, input_dim=5))
-rnn.add(SimpleRNNCell(8))
-rnn.add(GRUCell(10))
+@keras_test
+def test_sequential(	):
+    rnn = RecurrentSequential()
+    rnn.add(LSTMCell(7, input_dim=5))
+    rnn.add(SimpleRNNCell(8))
+    rnn.add(GRUCell(10))
 
 
-a = Input((7, 5))
-b = rnn(a)
+    a = Input((7, 5))
+    b = rnn(a)
 
-model = Model(a, b)
+    model = Model(a, b)
 
-model.compile(loss='mse', optimizer='sgd')
-model.fit((np.random.random((32, 7, 5))), np.random.random((32, 10)))
-model.predict(np.random.random((32, 7, 5)))
+    model.compile(loss='mse', optimizer='sgd')
+    model.fit((np.random.random((12, 7, 5))), np.random.random((12, 10)))
+    model.predict(np.random.random((12, 7, 5)))
 
-rnn = RecurrentSequential(state_initializer='random_normal')
-rnn.add(LSTMCell(7, input_dim=5))
-rnn.add(SimpleRNNCell(8))
-rnn.add(GRUCell(10))
-
-
-a = Input((7, 5))
-b = rnn(a)
-
-model = Model(a, b)
-
-model.compile(loss='mse', optimizer='sgd')
-model.fit((np.random.random((32, 7, 5))), np.random.random((32, 10)))
-model.predict(np.random.random((32, 7, 5)))
-
-rnn = RecurrentSequential(state_initializer=['random_normal', 'glorot_uniform'])
-rnn.add(LSTMCell(7, batch_input_shape=(32, 5)))
-rnn.add(SimpleRNNCell(8))
-rnn.add(GRUCell(10))
+@keras_test
+def test_state_initializer():
+    rnn = RecurrentSequential(state_initializer='random_normal')
+    rnn.add(LSTMCell(7, input_dim=5))
+    rnn.add(SimpleRNNCell(8))
+    rnn.add(GRUCell(10))
 
 
-a = Input((7, 5))
-b = rnn(a)
+    a = Input((7, 5))
+    b = rnn(a)
 
-model = Model(a, b)
+    model = Model(a, b)
 
-model.compile(loss='mse', optimizer='sgd')
-model.fit((np.random.random((32, 7, 5))), np.random.random((32, 10)))
-model.predict(np.random.random((32, 7, 5)))
+    model.compile(loss='mse', optimizer='sgd')
+    model.fit((np.random.random((12, 7, 5))), np.random.random((12, 10)))
+    model.predict(np.random.random((12, 7, 5)))
 
-
-
-rnn = RecurrentSequential(unroll=True)
-rnn.add(LSTMCell(7, input_dim=5))
-rnn.add(SimpleRNNCell(8))
-rnn.add(GRUCell(10))
-
-
-b = rnn(a)
-
-model = Model(a, b)
-
-model.compile(loss='mse', optimizer='sgd')
-model.fit((np.random.random((32, 7, 5))), np.random.random((32, 10)))
-model.predict(np.random.random((32, 7, 5)))
+@keras_test
+def test_state_initializer_as_list():
+    rnn = RecurrentSequential(state_initializer=['random_normal', 'glorot_uniform'])
+    rnn.add(LSTMCell(7, batch_input_shape=(12, 5)))
+    rnn.add(SimpleRNNCell(8))
+    rnn.add(GRUCell(10))
 
 
-rnn = RecurrentSequential(state_sync=True)
-rnn.add(LSTMCell(10, input_dim=5))
-rnn.add(LSTMCell(10))
-rnn.add(LSTMCell(10))
+    a = Input((7, 5))
+    b = rnn(a)
+
+    model = Model(a, b)
+
+    model.compile(loss='mse', optimizer='sgd')
+    model.fit((np.random.random((12, 7, 5))), np.random.random((12, 10)))
+    model.predict(np.random.random((12, 7, 5)))
+
+@keras_test
+def test_unroll():
+    rnn = RecurrentSequential(unroll=True)
+    rnn.add(LSTMCell(7, input_dim=5))
+    rnn.add(SimpleRNNCell(8))
+    rnn.add(GRUCell(10))
+
+    a = Input((7, 5))
+    b = rnn(a)
+
+    model = Model(a, b)
+
+    model.compile(loss='mse', optimizer='sgd')
+    model.fit((np.random.random((12, 7, 5))), np.random.random((12, 10)))
+    model.predict(np.random.random((12, 7, 5)))
+
+@keras_test
+def test_state_sync():
+    rnn = RecurrentSequential(state_sync=True)
+    rnn.add(LSTMCell(10, input_dim=5))
+    rnn.add(LSTMCell(10))
+    rnn.add(LSTMCell(10))
+
+    a = Input((7, 5))
+    b = rnn(a)
+
+    model = Model(a, b)
+
+    model.compile(loss='mse', optimizer='sgd')
+    model.fit((np.random.random((12, 7, 5))), np.random.random((12, 10)))
+    model.predict(np.random.random((12, 7, 5)))
 
 
-b = rnn(a)
+@keras_test
+def test_state_sync_unroll():
+    rnn = RecurrentSequential(state_sync=True, unroll=True)
+    rnn.add(LSTMCell(10, input_dim=5))
+    rnn.add(LSTMCell(10))
+    rnn.add(LSTMCell(10))
 
-model = Model(a, b)
+    a = Input((7, 5))
+    b = rnn(a)
 
-model.compile(loss='mse', optimizer='sgd')
-model.fit((np.random.random((32, 7, 5))), np.random.random((32, 10)))
-model.predict(np.random.random((32, 7, 5)))
+    model = Model(a, b)
 
-
-
-rnn = RecurrentSequential(state_sync=True, unroll=True)
-rnn.add(LSTMCell(10, input_dim=5))
-rnn.add(LSTMCell(10))
-rnn.add(LSTMCell(10))
-
-
-b = rnn(a)
-
-model = Model(a, b)
-
-model.compile(loss='mse', optimizer='sgd')
-model.fit((np.random.random((32, 7, 5))), np.random.random((32, 10)))
-model.predict(np.random.random((32, 7, 5)))
+    model.compile(loss='mse', optimizer='sgd')
+    model.fit((np.random.random((12, 7, 5))), np.random.random((12, 10)))
+    model.predict(np.random.random((12, 7, 5)))
 
 
 # Decoders
+@keras_test
+def test_decode():
+    a = Input((5,))
 
-a = Input((5,))
+    rnn = RecurrentSequential(decode=True, output_length=7)
+    rnn.add(LSTMCell(10, input_dim=5))
+    rnn.add(LSTMCell(10))
+    rnn.add(LSTMCell(10))
 
-rnn = RecurrentSequential(decode=True, output_length=7)
-rnn.add(LSTMCell(10, input_dim=5))
-rnn.add(LSTMCell(10))
-rnn.add(LSTMCell(10))
+    b = rnn(a)
 
-b = rnn(a)
+    model = Model(a, b)
 
-model = Model(a, b)
+    model.compile(loss='mse', optimizer='sgd')
+    model.fit((np.random.random((12, 5))), np.random.random((12, 7, 10)))
+    model.predict(np.random.random((12, 5)))
 
-model.compile(loss='mse', optimizer='sgd')
-model.fit((np.random.random((32, 5))), np.random.random((32, 7, 10)))
-model.predict(np.random.random((32, 5)))
+@keras_test
+def test_readout_state_sync():
+    a = Input((5,))
+    rnn = RecurrentSequential(state_sync=True, decode=True, output_length=7)
+    rnn.add(LSTMCell(10, input_dim=5))
+    rnn.add(LSTMCell(10))
+    rnn.add(LSTMCell(10))
 
+    b = rnn(a)
 
-rnn = RecurrentSequential(state_sync=True, decode=True, output_length=7)
-rnn.add(LSTMCell(10, input_dim=5))
-rnn.add(LSTMCell(10))
-rnn.add(LSTMCell(10))
+    model = Model(a, b)
 
-b = rnn(a)
+    model.compile(loss='mse', optimizer='sgd')
+    model.fit((np.random.random((12, 5))), np.random.random((12, 7, 10)))
+    model.predict(np.random.random((12, 5)))
 
-model = Model(a, b)
+@keras_test
+def test_decode_unroll():
+    a = Input((5,))
+    rnn = RecurrentSequential(decode=True, output_length=7, unroll=True)
+    rnn.add(LSTMCell(10, input_dim=5))
+    rnn.add(LSTMCell(10))
+    rnn.add(LSTMCell(10))
 
-model.compile(loss='mse', optimizer='sgd')
-model.fit((np.random.random((32, 5))), np.random.random((32, 7, 10)))
-model.predict(np.random.random((32, 5)))
+    b = rnn(a)
 
-rnn = RecurrentSequential(decode=True, output_length=7, unroll=True)
-rnn.add(LSTMCell(10, input_dim=5))
-rnn.add(LSTMCell(10))
-rnn.add(LSTMCell(10))
+    model = Model(a, b)
 
-b = rnn(a)
+    model.compile(loss='mse', optimizer='sgd')
+    model.fit((np.random.random((12, 5))), np.random.random((12, 7, 10)))
+    model.predict(np.random.random((12, 5)))
 
-model = Model(a, b)
+@keras_test
+def test_decode_unroll_state_sync():
+    a = Input((5,))
+    rnn = RecurrentSequential(state_sync=True, decode=True, output_length=7, unroll=True)
+    rnn.add(LSTMCell(10, input_dim=5))
+    rnn.add(LSTMCell(10))
+    rnn.add(LSTMCell(10))
 
-model.compile(loss='mse', optimizer='sgd')
-model.fit((np.random.random((32, 5))), np.random.random((32, 7, 10)))
-model.predict(np.random.random((32, 5)))
+    b = rnn(a)
 
-rnn = RecurrentSequential(state_sync=True, decode=True, output_length=7, unroll=True)
-rnn.add(LSTMCell(10, input_dim=5))
-rnn.add(LSTMCell(10))
-rnn.add(LSTMCell(10))
+    model = Model(a, b)
 
-b = rnn(a)
-
-model = Model(a, b)
-
-model.compile(loss='mse', optimizer='sgd')
-model.fit((np.random.random((32, 5))), np.random.random((32, 7, 10)))
-model.predict(np.random.random((32, 5)))
+    model.compile(loss='mse', optimizer='sgd')
+    model.fit((np.random.random((12, 5))), np.random.random((12, 7, 10)))
+    model.predict(np.random.random((12, 5)))
 
 
 # Readout
+@keras_test
+def test_readout():
+    a = Input((7, 5))
 
-a = Input((7, 5))
+    rnn = RecurrentSequential(readout=True)
+    rnn.add(LSTMCell(7, input_dim=5))
+    rnn.add(SimpleRNNCell(8))
+    rnn.add(GRUCell(5))
 
-rnn = RecurrentSequential(readout=True)
-rnn.add(LSTMCell(7, input_dim=5))
-rnn.add(SimpleRNNCell(8))
-rnn.add(GRUCell(5))
+    b = rnn(a)
 
-b = rnn(a)
+    model = Model(a, b)
 
-model = Model(a, b)
+    model.compile(loss='mse', optimizer='sgd')
+    model.fit((np.random.random((12, 7, 5))), np.random.random((12, 5)))
+    model.predict(np.random.random((12, 7, 5)))
 
-model.compile(loss='mse', optimizer='sgd')
-model.fit((np.random.random((32, 7, 5))), np.random.random((32, 5)))
-model.predict(np.random.random((32, 7, 5)))
+@keras_test
+def test_readout_unroll():
+    a = Input((7, 5))
+    rnn = RecurrentSequential(readout=True, unroll=True)
+    rnn.add(LSTMCell(7, input_dim=5))
+    rnn.add(SimpleRNNCell(8))
+    rnn.add(GRUCell(5))
 
-rnn = RecurrentSequential(readout=True, unroll=True)
-rnn.add(LSTMCell(7, input_dim=5))
-rnn.add(SimpleRNNCell(8))
-rnn.add(GRUCell(5))
+    b = rnn(a)
 
-b = rnn(a)
+    model = Model(a, b)
 
-model = Model(a, b)
+    model.compile(loss='mse', optimizer='sgd')
+    model.fit((np.random.random((12, 7, 5))), np.random.random((12, 5)))
+    model.predict(np.random.random((12, 7, 5)))
 
-model.compile(loss='mse', optimizer='sgd')
-model.fit((np.random.random((32, 7, 5))), np.random.random((32, 5)))
-model.predict(np.random.random((32, 7, 5)))
+@keras_test
+def test_readout_state_sync():
+    a = Input((7, 5))
+    rnn = RecurrentSequential(readout=True, state_sync=True)
+    rnn.add(LSTMCell(5, input_dim=5))
+    rnn.add(LSTMCell(5))
+    rnn.add(LSTMCell(5))
 
-rnn = RecurrentSequential(readout=True, state_sync=True)
-rnn.add(LSTMCell(5, input_dim=5))
-rnn.add(LSTMCell(5))
-rnn.add(LSTMCell(5))
+    b = rnn(a)
 
-b = rnn(a)
+    model = Model(a, b)
 
-model = Model(a, b)
+    model.compile(loss='mse', optimizer='sgd')
+    model.fit((np.random.random((12, 7, 5))), np.random.random((12, 5)))
+    model.predict(np.random.random((12, 7, 5)))
 
-model.compile(loss='mse', optimizer='sgd')
-model.fit((np.random.random((32, 7, 5))), np.random.random((32, 5)))
-model.predict(np.random.random((32, 7, 5)))
+@keras_test
+def test_readout_state_sync_unroll():
+    a = Input((7, 5))
+    rnn = RecurrentSequential(readout=True, state_sync=True, unroll=True)
+    rnn.add(LSTMCell(5, input_dim=5))
+    rnn.add(LSTMCell(5))
+    rnn.add(LSTMCell(5))
 
+    b = rnn(a)
 
-rnn = RecurrentSequential(readout=True, state_sync=True, unroll=True)
-rnn.add(LSTMCell(5, input_dim=5))
-rnn.add(LSTMCell(5))
-rnn.add(LSTMCell(5))
+    model = Model(a, b)
 
-b = rnn(a)
-
-model = Model(a, b)
-
-model.compile(loss='mse', optimizer='sgd')
-model.fit((np.random.random((32, 7, 5))), np.random.random((32, 5)))
-model.predict(np.random.random((32, 7, 5)))
+    model.compile(loss='mse', optimizer='sgd')
+    model.fit((np.random.random((12, 7, 5))), np.random.random((12, 5)))
+    model.predict(np.random.random((12, 7, 5)))
 
 
 
 # Decoder + readout
+@keras_test
+def test_decoder_readout():
+    a = Input((5,))
 
-a = Input((5,))
+    rnn = RecurrentSequential(decode=True, output_length=7, readout=True)
+    rnn.add(LSTMCell(5, input_dim=5))
+    rnn.add(LSTMCell(5))
+    rnn.add(LSTMCell(5))
 
-rnn = RecurrentSequential(decode=True, output_length=7, readout=True)
-rnn.add(LSTMCell(5, input_dim=5))
-rnn.add(LSTMCell(5))
-rnn.add(LSTMCell(5))
+    b = rnn(a)
 
-b = rnn(a)
+    model = Model(a, b)
 
-model = Model(a, b)
-
-model.compile(loss='mse', optimizer='sgd')
-model.fit((np.random.random((32, 5))), np.random.random((32, 7, 5)))
-model.predict(np.random.random((32, 5)))
+    model.compile(loss='mse', optimizer='sgd')
+    model.fit((np.random.random((12, 5))), np.random.random((12, 7, 5)))
+    model.predict(np.random.random((12, 5)))
 
 # teacher forcing
+@keras_test
+def test_teacher_force():
+    a = Input((7, 5))
 
-a = Input((7, 5))
+    rnn = RecurrentSequential(readout=True, teacher_force=True)
+    rnn.add(LSTMCell(7, input_dim=5))
+    rnn.add(SimpleRNNCell(8))
+    rnn.add(GRUCell(5))
 
-rnn = RecurrentSequential(readout=True, teacher_force=True)
-rnn.add(LSTMCell(7, input_dim=5))
-rnn.add(SimpleRNNCell(8))
-rnn.add(GRUCell(5))
+    ground_truth = Input((7, 5))
 
-ground_truth = Input((7, 5))
+    b = rnn(a, ground_truth=ground_truth)
 
-b = rnn(a, ground_truth=ground_truth)
+    model = Model([a, ground_truth], b)
 
-model = Model([a, ground_truth], b)
-
-model.compile(loss='mse', optimizer='sgd')
-model.fit([np.random.random((32, 7, 5)), np.random.random((32, 7, 5))], np.random.random((32, 5)))
-model.predict([np.random.random((32, 7, 5))] * 2)
+    model.compile(loss='mse', optimizer='sgd')
+    model.fit([np.random.random((12, 7, 5)), np.random.random((12, 7, 5))], np.random.random((12, 5)))
+    model.predict([np.random.random((12, 7, 5))] * 2)
--- tests/test_rnn_cell_from_model.py
+++ tests/test_rnn_cell_from_model.py
@@ -1,24 +1,29 @@
-from recurrentshop import *
-from keras.models import *
-from keras.layers import *
+from recurrentshop import RecurrentSequential, RNNCellFromModel
+from keras.models import Model
+from keras.layers import add, Activation, Dense, Input
+from keras.utils.test_utils import keras_test
+import numpy as np
 
-x = Input((5,))
-h_tm1 = Input((10,))
-h = add([Dense(10)(x), Dense(10, use_bias=False)(h_tm1)])
-h = Activation('tanh')(h)
 
-cell_model = Model([x, h_tm1], [h, h])
+@keras_test
+def test_rnn_cell_from_model():
+    x = Input((5,))
+    h_tm1 = Input((10,))
+    h = add([Dense(10)(x), Dense(10, use_bias=False)(h_tm1)])
+    h = Activation('tanh')(h)
 
-rnn_cell = RNNCellFromModel(cell_model)
+    cell_model = Model([x, h_tm1], [h, h])
 
-rnn = RecurrentSequential()
-rnn.add(rnn_cell)
+    rnn_cell = RNNCellFromModel(cell_model)
 
-a = Input((7, 5))
-b = rnn(a)
+    rnn = RecurrentSequential()
+    rnn.add(rnn_cell)
 
-model = Model(a, b)
+    a = Input((7, 5))
+    b = rnn(a)
 
-model.compile(loss='mse', optimizer='sgd')
-model.fit((np.random.random((32, 7, 5))), np.random.random((32, 10)))
-model.predict(np.zeros((32, 7, 5)))
+    model = Model(a, b)
+
+    model.compile(loss='mse', optimizer='sgd')
+    model.fit((np.random.random((32, 7, 5))), np.random.random((32, 10)))
+    model.predict(np.zeros((32, 7, 5)))
